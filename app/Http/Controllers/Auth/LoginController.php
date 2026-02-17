ee4d1e496a85196cc97c365947d3b1ce1aa2d95c
iaiapps <ikromudin.ai@gmail.com>
2026-02-17 12:21:14 +0700

    fix login siswa

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,14 +41,34 @@ class LoginController extends Controller
     }
 
     /**
+     * Validate the user login request.
+     */
+    protected function validateLogin(Request $request)
+    {
+        if ($request->has('nis') && $request->has('kode_guru')) {
+            $request->validate([
+                'nis' => 'required|string',
+                'kode_guru' => 'required|string',
+                'password' => 'required|string',
+            ]);
+        } else {
+            $request->validate([
+                'email' => 'required|string',
+                'password' => 'required|string',
+            ]);
+        }
+    }
+
+    /**
      * Attempt to log the user into the application.
      * Support login dengan NIS atau email
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return bool
      */
     protected function attemptLogin(Request $request)
     {
+
+        // dd($request->all());
         // 1. Khusus Login Siswa (NIS + Kode Sekolah)
         if ($request->has('nis') && $request->has('kode_guru')) {
             $nis = $request->input('nis');
@@ -60,7 +80,7 @@ class LoginController extends Controller
 
             if ($teacherId) {
                 // Rangkai ulang menjadi email autentikasi: nis.teacherId@amal.web.id
-                $email = $nis . '.' . $teacherId . '@amal.web.id';
+                $email = $nis.'.'.$teacherId.'@amal.web.id';
 
                 return $this->guard()->attempt(
                     ['email' => $email, 'password' => $password],
